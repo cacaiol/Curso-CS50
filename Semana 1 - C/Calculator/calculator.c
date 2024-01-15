4661b8cf464ecfb0a8c098f5eff4e9b6a11e6bcf
Caio <caiobart601@gmail.com>
2024-01-15 12:12:35 -0300

    Organizando os exercicios 1º semana

--- Semana 1 - C/Calculator/calculator.c
+++ Semana 1 - C/Calculator/calculator.c
@@ -1,18 +1,12 @@
  #include <cs50.h>
  #include <stdio.h>
 
- int add(int a, int b);
-
  int main(void)
  {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", add(x, y));
- }
-
- int add(int a, int b)
- {
-    return a + b;
+   double z = (double) x / (double) y;
+   printf("%f\n", z);
  }
 
